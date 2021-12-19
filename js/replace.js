f6dcf3fa080bfe7570f306b8f72f0daa1ec63b24
FAISAL Mohammad <1015900+faisal6621@users.noreply.github.com>
2021-12-19 20:50:51 +0530

    💄 marking for list and anchors

--- js/replace.js
+++ js/replace.js
@@ -8,8 +8,6 @@ const doneMarking = total => {
 }
 
 const marked = markedDOM => {
-    console.log("marked:", markedDOM.innerText)
-    // update the markedDOM to add meaning to the marked worked
     const description = words[markedDOM.innerText.toLowerCase()]
     const meaningSpan = document.createElement("span")
     meaningSpan.className = "meaning"
@@ -29,40 +27,40 @@ const markOptions = {
 window.onload = (event) => {
     console.log("window loaded")
 
-    const span = document.createElement("span")
-    span.setAttribute("id", "replacer")
-    span.style.display = "none"
-    document.body.appendChild(span)
-    console.log('document updated')
+    // const span = document.createElement("span")
+    // span.setAttribute("id", "replacer")
+    // span.style.display = "none"
+    // document.body.appendChild(span)
+    // console.log('document updated')
 
-    const markInstance = new Mark(document.querySelectorAll("p"))
+    const markInstance = new Mark(document.querySelectorAll("p, li, a"))
     for (var word in words) {
         markInstance.mark(word, markOptions)
     }
 }
 
-window.onmouseup = (event) => {
-    const selection = window.getSelection()
-    const range = selection.getRangeAt(0)
-    const rect = range.getBoundingClientRect()
-    const text = selection.toString()
-    if (text.length > 0) {
-        const span = document.getElementById("replacer")
-        span.style.display = "block"
-        span.style.position = "fixed"
-        span.style.left = rect.left + "px"
-        span.style.top = rect.top + rect.height + "px"
-        span.style.backgroundColor = "rgba(200, 180, 255, 0.5)"
-        span.style.border = "1px solid black"
-        span.style.padding = "5px"
-        span.style.zIndex = "9999"
-        span.innerHTML = text + " selected"
-    } else {
-        const span = document.getElementById("replacer")
-        span.style.display = "none"
-    }
+// window.onmouseup = (event) => {
+//     const selection = window.getSelection()
+//     const range = selection.getRangeAt(0)
+//     const rect = range.getBoundingClientRect()
+//     const text = selection.toString()
+//     if (text.length > 0) {
+//         const span = document.getElementById("replacer")
+//         span.style.display = "block"
+//         span.style.position = "fixed"
+//         span.style.left = rect.left + "px"
+//         span.style.top = rect.top + rect.height + "px"
+//         span.style.backgroundColor = "rgba(200, 180, 255, 0.5)"
+//         span.style.border = "1px solid black"
+//         span.style.padding = "5px"
+//         span.style.zIndex = "9999"
+//         span.innerHTML = text + " selected"
+//     } else {
+//         const span = document.getElementById("replacer")
+//         span.style.display = "none"
+//     }
 
-}
+// }
 
 const getResource = (path) => {
     return chrome.runtime.getURL(path)
